60a85131ed881564c880303ac70b4f349d5d1869
huzejun <huzejun1990>
2022-10-20 02:23:25 +0800

    循环结构 do while for 2022-10-20 02:23:18

---
+++ cicularstructure/01_while_loop.cpp
@@ -0,0 +1,53 @@
+/*
+* Created by huzejun on 2022/10/20 0:44
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //while循环
+    //在屏幕中打印 0~9 这10个数字
+//    int num = 0;
+//    cout << num << endl;
+
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+//
+//    num++;
+//    cout << num << endl;
+
+    int num = 0;
+
+    //while () 中填入循环条件
+    while (num < 10) {
+        cout << num << endl;
+        num++;
+    }
+
+
+    system("pause");
+
+    return 0;
+}
